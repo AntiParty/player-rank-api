8d0a78347e937ae729b771a21481b3a5094a6d89
Antiparty <75674175+AntiParty@users.noreply.github.com>
2025-01-31 08:33:03 -0700

    Update index.js

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ async function fetchPlayerRank() {
     const apiUrl = "https://api.rivalstracker.com/api/player/2118492390?season=2";
     const fullUrl = `${proxyUrl}${encodeURIComponent(apiUrl)}`; // Combine proxy and API URL
     const rankMapping = {
-        1: "Bronze 1", 2: "Bronze 2", 3: "Bronze 3",
-        4: "Silver 1", 5: "Silver 2", 6: "Silver 3",
-        7: "Gold 1", 8: "Gold 2", 9: "Gold 3",
-        10: "Platinum 1", 11: "Platinum 2", 12: "Platinum 3",
-        13: "Diamond 1", 14: "Diamond 2", 15: "Diamond 3",
-        16: "Grandmaster 1", 17: "Grandmaster 2", 18: "Grandmaster 3",
-        19: "Celestial 1", 20: "Celestial 2", 21: "Celestial 3",
+        1: "Bronze 3", 2: "Bronze 2", 3: "Bronze 1",
+        4: "Silver 3", 5: "Silver 2", 6: "Silver 1",
+        7: "Gold 3", 8: "Gold 2", 9: "Gold 1",
+        10: "Platinum 3", 11: "Platinum 2", 12: "Platinum 1",
+        13: "Diamond 3", 14: "Diamond 2", 15: "Diamond 1",
+        16: "Grandmaster 3", 17: "Grandmaster 2", 18: "Grandmaster 1",
+        19: "Celestial 3", 20: "Celestial 2", 21: "Celestial 1",
         22: "Eternity", 23: "One Above All"
     };
 
